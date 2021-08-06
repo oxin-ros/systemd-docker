84e6dd9d97d8bea722822d30f577adbfaf9578a3
Thomas Willetal <t.willetal@googlemail.com>
2021-08-06 21:41:35 +0200

    devcontainer

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install --yes $(dpkg-checkbuilddeps 2>&1 | sed -e 's/dpkg-checkbuilddeps:\serror:\sUnmet build dependencies: //g' -e  's/[\(][^)]*[\)] //g')
